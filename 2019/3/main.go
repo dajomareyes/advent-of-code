a7a5b142dfc1dec04d39bc1d25b8d01ed336eaf1
David Joshua Reyes <davidjoshuareyes@gmail.com>
2021-04-30 00:32:17 -0400

    Updates to 2019/3

--- 2019/3/main.go
+++ 2019/3/main.go
@@ -18,6 +18,25 @@ type Line struct {
 	x     float64
 }
 
+type LineEquation struct {
+	A float64
+	B float64
+	C float64
+}
+
+func findIntersectionCoordinates(l1, l2 LineEquation) (Coordinate, error) {
+	determinant := l1.A*l2.B - l2.A*l1.B
+	if determinant == 0 {
+		// lines are parallel
+		return Coordinate{}, errors.New("The lines are parallel")
+	}
+
+	x := (l2.B*l1.C - l1.B*l2.C) / determinant
+	y := (l1.A*l2.C - l2.A*l1.C) / determinant
+
+	return Coordinate{x, y}, nil
+}
+
 func findIntersection(l1, l2 Line) (Coordinate, error) {
 	if l1.slope == l2.slope {
 		return Coordinate{}, errors.New("The lines don't intersect")
@@ -71,6 +90,13 @@ func generatePath(path []string) []Coordinate {
 	return coordPath
 }
 
+func createLineEquation(a, b Coordinate) LineEquation {
+	A := b.y - a.y
+	B := a.x - b.x
+	C := A*a.x + B*b.y
+	return LineEquation{A, B, C}
+}
+
 func createLine(a, b Coordinate) Line {
 	slope := (a.y - b.y) / (a.x - b.x)
 	yint := a.y - slope*a.x
@@ -83,22 +109,51 @@ func createLine(a, b Coordinate) Line {
 	return result
 }
 
+func createLines(path []Coordinate) []LineEquation {
+	var lines []LineEquation
+	for i := 0; i < len(path)-1; i++ {
+		j := i + 1
+		lines = append(lines, createLineEquation(path[i], path[j]))
+	}
+	return lines
+}
+
 func main() {
 	fmt.Println("Starting application")
-	path1 := strings.Split("R75,D30,R83,U83,L12,D49,R71,U7,L72", ",")
-	path2 := strings.Split("U62,R66,U55,R34,D71,R55,D58,R83", ",")
-	fmt.Println(path1, path2)
-	cords1 := generatePath(path1)
-	cords2 := generatePath(path2)
 
-	fmt.Println(cords1)
-	fmt.Println(cords2)
-	// TODO: Find intersections between path
+	// generate paths from input
+	// example 2
+	//path1 := generatePath(strings.Split("R75,D30,R83,U83,L12,D49,R71,U7,L72", ","))
+	//path2 := generatePath(strings.Split("U62,R66,U55,R34,D71,R55,D58,R83", ","))
 
-	l1 := createLine(Coordinate{0, 0}, Coordinate{0, 75})
-	l2 := createLine(Coordinate{75, 2}, Coordinate{-75, 2})
-	fmt.Println(l1)
-	fmt.Println(l2)
+	// example 1
+	path1 := generatePath(strings.Split("R8,U5,L5,D3", ","))
+	path2 := generatePath(strings.Split("U7,R6,D4,L4", ","))
 
-	fmt.Println(findIntersection(l1, l2))
+	// generate lines
+	lines1 := createLines(path1)
+	lines2 := createLines(path2)
+
+	var intersections []Coordinate
+
+	for i := 0; i < len(lines1); i++ {
+		for j := 0; j < len(lines2); j++ {
+			coord, err := findIntersectionCoordinates(lines1[i], lines2[j])
+			if err != nil {
+				continue
+			}
+			intersections = append(intersections, coord)
+		}
+	}
+
+	// TODO: intersesctions are coming out wrong because it things that each of the lines are infinite
+	// you might want to look into using vectors instead since vectors have magnitude
+	fmt.Println(intersections)
+
+	// l1 := createLineEquation(Coordinate{0, 0}, Coordinate{75, 0})
+	// l2 := createLineEquation(Coordinate{0, 0}, Coordinate{0, 62})
+	// fmt.Println(l1)
+	// fmt.Println(l2)
+
+	// fmt.Println(findIntersectionCoordinates(l1, l2))
 }
